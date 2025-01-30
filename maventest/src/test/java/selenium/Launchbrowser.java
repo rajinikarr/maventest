be55568d1b547df9fcf2b4c58088827981f84a64
Rajinikar <rajinikar10@gmail.com>
2025-01-30 13:20:48 +0000

    First commit

---
+++ maventest/src/test/java/selenium/Launchbrowser.java
@@ -0,0 +1,18 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Launchbrowser {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			WebDriverManager.chromedriver().setup();
+			WebDriver driver = new ChromeDriver ();
+			driver.manage().window().maximize();
+			driver.get("https://www.google.co.uk/");
+			driver.close();
+		}
+
+	}
